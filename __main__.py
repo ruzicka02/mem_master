53a2a85928215413a4b9237e42cb2ba2eedaadce
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2024-02-04 19:36:55 +0100

    Read, read_range

--- __main__.py
+++ __main__.py
@@ -3,6 +3,8 @@
 import struct
 import click
 import subprocess
+import os
+import sys
 
 BASE_ADDR = int("0xa000_0000", base=16)
 
@@ -29,6 +31,39 @@ def encode(num):
 @click.argument('hex_string')
 def decode(hex_string):
     click.echo(hex2float(hex_string))
+
+@cli.command()
+@click.argument('index', type=int)
+def read(index):
+    if os.geteuid() != 0:
+        click.echo("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
+        sys.exit(1)
+
+    addr: str = hex(BASE_ADDR + 4 * index)
+    click.echo(f"[DEBUG] Running `sudo devmem2m {addr}`")
+
+    try:
+        proc = subprocess.run(["sudo", "devmem2m", addr], capture_output=True, timeout=5)
+    except subprocess.TimeoutExpired as exc:
+        click.echo(f"Timeout of the reading process. Aborting.")
+        click.echo(f"Provided stderr:\n{exc.stderr.decode('utf-8')}")
+        sys.exit(2)
+
+    if proc.returncode != 0:
+        click.echo(f"Problem occured; return code {proc.returncode}")
+        click.echo(f"Provided stderr:\n{proc.stderr.decode('utf-8')}")
+        sys.exit(3)
+
+    mem_content = proc.stdout.decode("utf-8")
+    click.echo(hex2float(mem_content))
+
+@cli.command()
+@click.argument('start', type=int)
+@click.argument('end', type=int)
+def read_range(start, end):
+    for i in range(start, end):
+        click.echo(f"[{i:<2d}] ", nl=False)  # index info
+        read(i)  # this automatically dumps the memory value
 
 
 if __name__ == "__main__":
